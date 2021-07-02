78dac0f069622fa4d7bfd71fea50c4486a96aea6
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-07-02 02:31:07 +0100

    commiting

--- src/components/expenses/ExpenseDate.jsx
+++ src/components/expenses/ExpenseDate.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import './expenses.sass'
 
 function ExpenseDate(props) {
 
   const month = props.date.toLocaleString('en-US', {month: 'long'});
   const day = props.date.toLocaleString('en-US', {day: '2-digit'});
   const year = props.date.getFullYear();
+
   return (
      <div>
-       <div>
-         <div>{month}</div>
-         <div>{day}</div>
-         <div>{year}</div>
+       <div className="expense-date">
+         <div className="expense-date_month">{month}</div>
+         <div className="expense-date_day">{day}</div>
+         <div className="expense-date_year">{year}</div>
        </div>
      </div>
   );
--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import './sass/ExpenseItem.sass'
+import './expenses.sass'
 import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
   return (
      <div className="expense-item">
        <ExpenseDate date={props.date}/>
+
        <div className="expense-item_description">
          <h2>{props.title}</h2>
        </div>
+
        <div className="expense-item__price">
          ${props.amount}
        </div>
