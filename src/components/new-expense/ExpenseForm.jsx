eae1eb98c50b0a17f05a4247d97329ea375ed92e
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-07-03 20:41:45 +0100

    add event listener

--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -1,22 +1,53 @@
 import './expense-form.sass';
+import { useState } from 'react';
 
 
 const Expenseform = () => {
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  }
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  }
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  }
+
+
   return (
     <form>
 
       <div className="new-expense_controls">
         <div className="new-expense_control">
           <label>Title</label>
-          <input type="text"/>
+          <input
+            type="text"
+            onChange={titleChangeHandler} />
         </div>
+
         <div className="new-expense_control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler} />
         </div>
+
         <div className="new-expense_control">
           <label>Date</label>
-          <input type="date" min="2020-01-01" max="2030-12-31"/>
+          <input
+            type="date"
+            min="2020-01-01"
+            max="2030-12-31"
+            onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense_actions">
