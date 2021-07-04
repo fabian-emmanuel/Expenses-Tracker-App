5a9e5266aa3cd7131ec08d055653302101db602f
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-07-04 20:56:11 +0100

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import './sass/App.css';
 import Expenses from "./components/expenses/Expenses";
 import NewExpense from './components/new-expense/NewExpense';
+import {useState} from 'react';
 
-function App() {
-  const expenses = [
-    {
-      id: 1,
-      title: 'Car Insurance',
-      amount: 356.76,
-      date: new Date(2021, 5, 12)
-    },
-    {
-      id: 2,
-      title: 'Health Insurance',
-      amount: 546.44,
-      date: new Date(2021, 6, 15)
-    },
-    {
-      id: 3,
-      title: 'Food Insurance',
-      amount: 609.78,
-      date: new Date(2021, 7, 2)
-    }
-  ];
-
-  const addExpenseHandler = expense => {
-    console.log("In App.js");
-    console.log(expense)
+const DUMMY_EXPENSES = [{
+    id: 1,
+    title: 'Car Insurance',
+    amount: 356.76,
+    date: new Date(2021, 5, 12)
+  },
+  {
+    id: 2,
+    title: 'Health Insurance',
+    amount: 546.44,
+    date: new Date(2021, 6, 15)
+  },
+  {
+    id: 3,
+    title: 'Food Insurance',
+    amount: 609.78,
+    date: new Date(2021, 7, 2)
   }
+];
+
+const App = () => {
+ const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
 
 
